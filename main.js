da550cd53648efc3b4fb3b69e772a18d1e20da80
Christopher Mendoza <tiptop1112@gmail.com>
2024-06-23 17:49:06 -0700

    Created function to return rock paper or scissors Used if/ else if statements to conditionally have the computer return rock paper or scissors based on the value returned by using Math.random() method

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello World!')
+// psuedocode
+// Step 1: Write logic to get the computers choice
+//  . create a function called getComputerChoice
+//  . function should return one of the following string values: `rock`, `paper` or `scissors`
+//  .  use random method conditionally to return one of the string values
+//  . if equal to 0 return rock
+//  . if equal to 1 return paper
+//  . if equal to 2 return scissor
+function getComputerChoice () {
+    let random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        return `${random} rock`
+    } else if (random === 1) {
+        return `${random} paper`
+    } else if(random === 2) {
+        return `${random} scissors`}
+}
